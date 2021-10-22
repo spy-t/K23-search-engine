fc362f2e31833b8cd2df0c309abdb9964b19f25a
Spyros Trifonidis <spyrostrifon@protonmail.com>
2021-10-22 13:23:32 +0300

    specialize optional to improve types behind pointer handling

--- include/qs/optional.hpp
+++ include/qs/optional.hpp
@@ -1,8 +1,14 @@
 #ifndef QS_OPTIONAL_H
 #define QS_OPTIONAL_H
+#include <stdexcept>
+#include <type_traits>
+
 namespace qs {
 
-template <typename T> class optional {
+template <typename T, bool = std::is_pointer_v<T>> class optional {
+
+  using value_type = typename std::remove_pointer<T>::type;
+
 private:
   struct empty_marker {};
   union {
@@ -16,11 +22,57 @@ public:
   explicit optional(T val) : value(val), has_value(true) {}
   static optional<T> empty() { return optional(); }
 
-  T get_or(T default_) {
+  T get_or(value_type &&default_) {
     if (has_value) {
+      has_value = false;
       return value;
     } else {
-      return default_;
+      T boxed_default = new value_type(default_);
+      return boxed_default;
+    }
+  }
+
+  T get() {
+    if (has_value) {
+      has_value = false;
+      return value;
+    } else {
+      throw std::runtime_error("Bad optional access");
+    }
+  }
+
+  bool is_empty() { return !has_value; }
+};
+
+template <typename T> class optional<T, false> {
+private:
+  struct empty_marker {};
+  union {
+    empty_marker e;
+    T value;
+  };
+  bool has_value;
+
+public:
+  explicit optional() : e(), has_value(false) {}
+  explicit optional(T val) : value(val), has_value(true) {}
+  static optional<T> empty() { return optional(); }
+
+  T &&get_or(T &&default_) {
+    if (has_value) {
+      has_value = false;
+      return std::move(value);
+    } else {
+      return std::move(default_);
+    }
+  }
+
+  T &&get() {
+    if (has_value) {
+      has_value = false;
+      return std::move(value);
+    } else {
+      throw std::runtime_error("Bad optional access");
     }
   }
 
--- src/bin/search.cpp
+++ src/bin/search.cpp
@@ -6,15 +6,15 @@ int main() {
 
   qs::optional<int> nullopt;
 
-  std::cout << "Primitive optional with value: " << opt.get_or(0) << "\n";
-  std::cout << "Primitive optional without value: " << nullopt.is_empty()
+  std::cout << "Primitive optional with value: " << opt.get() << "\n";
+  std::cout << "Primitive optional without value: " << nullopt.get_or(9999)
             << "\n";
 
   int *int_ptr = new int(5);
   qs::optional<int *> ptr_opt(int_ptr);
   qs::optional<int *> ptr_nullopt;
 
-  std::cout << "Pointer optional with value: " << *ptr_opt.get_or(0) << "\n";
-  std::cout << "Pointer optional without value: " << ptr_nullopt.is_empty()
+  std::cout << "Pointer optional with value: " << *ptr_opt.get() << "\n";
+  std::cout << "Pointer optional without value: " << *ptr_nullopt.get_or(9999)
             << "\n";
 }
