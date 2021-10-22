00c84d66bca980fbeb746625330b20b36f1e69cc
Spyros Trifonidis <spyrostrifon@protonmail.com>
2021-10-22 12:56:16 +0300

    implement an optional container

--- include/qs/optional.hpp
+++ include/qs/optional.hpp
@@ -1,4 +1,31 @@
 #ifndef QS_OPTIONAL_H
 #define QS_OPTIONAL_H
-namespace qs {}
+namespace qs {
+
+template <typename T> class optional {
+private:
+  struct empty_marker {};
+  union {
+    empty_marker e;
+    T value;
+  };
+  bool has_value;
+
+public:
+  explicit optional() : e(), has_value(false) {}
+  explicit optional(T val) : value(val), has_value(true) {}
+  static optional<T> empty() { return optional(); }
+
+  T get_or(T default_) {
+    if (has_value) {
+      return value;
+    } else {
+      return default_;
+    }
+  }
+
+  bool is_empty() { return !has_value; }
+};
+
+} // namespace qs
 #endif
--- src/bin/search.cpp
+++ src/bin/search.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-#include <qs/fs.h>
-#include <qs/vector.hpp>
+#include <qs/optional.hpp>
 
 int main() {
-  qs::vector<int> v(400);
+  qs::optional<int> opt(5);
 
-  v.push(1);
+  qs::optional<int> nullopt;
 
-  std::cout << v.get_size() << "\n";
-  std::cout << qs::get_basename("/src/basename") << "\n";
+  std::cout << "Primitive optional with value: " << opt.get_or(0) << "\n";
+  std::cout << "Primitive optional without value: " << nullopt.is_empty()
+            << "\n";
+
+  int *int_ptr = new int(5);
+  qs::optional<int *> ptr_opt(int_ptr);
+  qs::optional<int *> ptr_nullopt;
+
+  std::cout << "Pointer optional with value: " << *ptr_opt.get_or(0) << "\n";
+  std::cout << "Pointer optional without value: " << ptr_nullopt.is_empty()
+            << "\n";
 }
