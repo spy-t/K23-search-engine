7959c934877ccbd927e669a84c19d3af5f097c01
Spyros Trifonidis <spyrostrifon@protonmail.com>
2021-10-27 17:54:56 +0300

    use bdd style tests

--- src/test/string_test.cpp
+++ src/test/string_test.cpp
@@ -2,11 +2,19 @@
 
 #include <qs/string.h>
 
-TEST_CASE("String concatenation") {
-  qs::string s1((char *)"String 1");
-  qs::string s2((char *)"String 2");
+SCENARIO("String sizes work when creating and concatenating") {
+  GIVEN("Two strings") {
+    qs::string s1((char *)"String 1");
+    qs::string s2((char *)"String 2");
+    REQUIRE(s1.get_size() == 8);
+    REQUIRE(s2.get_size() == 8);
 
-  auto s3 = s1 + s2;
+    WHEN("They are concatenated") {
+      auto s3 = s1 + s2;
 
-  REQUIRE(s3.get_size() == 16);
+      THEN("The resulting string has their summed up size") {
+        REQUIRE(s3.get_size() == s1.get_size() + s2.get_size());
+      }
+    }
+  }
 }
