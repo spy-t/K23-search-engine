aeac318190329e1a869cf115edb4165f0612481b
ythomop <ythomop@gmail.com>
2021-10-31 13:48:25 +0200

    Make qs::binary_search() simpler

--- include/qs/search.hpp
+++ include/qs/search.hpp
@@ -5,16 +5,17 @@
 
 namespace qs {
 
-template <class T> int binary_search(T data[], std::size_t size, T search_elem) {
+template <class T>
+int binary_search(T data[], std::size_t size, T search_elem) {
   int left = 0;
   int right = int(size) - 1;
   int middle;
 
   while (left <= right) {
     middle = (left + right) / 2;
-    if (qs::functions::deref(data[middle]) < qs::functions::deref(search_elem)) {
+    if (data[middle] < search_elem) {
       left = middle + 1;
-    } else if (qs::functions::deref(data[middle]) > qs::functions::deref(search_elem)) {
+    } else if (data[middle] > search_elem) {
       right = middle - 1;
     } else {
       return middle;
