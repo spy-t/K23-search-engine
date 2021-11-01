70430d15e865ea2a2363b16162dc509ffd554f36
ismagrip <christos@niovity.com>
2021-11-01 16:33:28 +0200

    formatting

--- include/qs/hash_set.hpp
+++ include/qs/hash_set.hpp
@@ -2,30 +2,22 @@
 #define QS_HASH_SET_HPP
 
 #include <qs/hash_table.hpp>
-#include <qs/string.h>
 #include <qs/optional.hpp>
+#include <qs/string.h>
 
 namespace qs {
-template <typename V> class hash_set{
+template <typename V> class hash_set {
   hash_table<V> table;
 
 public:
-  hash_set(){
-    table = hash_table<V>();
-  }
-  ~hash_set(){
-    delete table;
-  }
+  hash_set() { table = hash_table<V>(); }
+  ~hash_set() { delete table; }
 
-  void insert(qs::string key){
-    table.insert(key,key);
-  }
+  void insert(qs::string key) { table.insert(key, key); }
 
-  void remove(qs::string key){
-    table.remove(key);
-  }
+  void remove(qs::string key) { table.remove(key); }
 
-  bool contains(qs::string key){
+  bool contains(qs::string key) {
     qs::optional<V> val = table.lookup(key);
     return !val.is_empty();
   }
