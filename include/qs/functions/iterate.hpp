dea7238c252b8aadda0f2a5720c2eba3a2426ef6
ismagrip <cmagripis@protonmail.com>
2021-11-03 18:30:20 +0200

    for each over iterators

---
+++ include/qs/functions/iterate.hpp
@@ -0,0 +1,15 @@
+#ifndef QS_ITERATE_HPP
+#define QS_ITERATE_HPP
+
+
+namespace qs {
+namespace functions {
+template <typename Iterator,typename F> void for_each(Iterator begin,Iterator end,F fn) {
+  for (; begin != end; begin++) {
+      fn(*begin);
+  }
+}
+
+} // namespace functions
+} // namespace qs
+#endif // QS_ITERATE_HPP
