42bc3102952206f7929107fc8cc7406df3300d42
ismagrip <cmagripis@protonmail.com>
2021-11-03 12:01:10 +0200

    added test cases for removal and get_all

--- src/test/hash_set_test.cpp
+++ src/test/hash_set_test.cpp
@@ -2,7 +2,7 @@
 
 #include <qs/hash_set.h>
 
-TEST_CASE("Hash insertion") {
+TEST_CASE("Hash set insertion and removal") {
   GIVEN("A hash set and a key (qs::string)") {
     qs::hash_set set;
     qs::string key("key");
@@ -11,6 +11,41 @@ TEST_CASE("Hash insertion") {
       set.insert(key);
       THEN("The key must be inside the hash set") {
         REQUIRE(true == set.contains(key));
+      }
+    }
+    WHEN("We insert the key and then remove it.") {
+      set.insert(key);
+      set.remove(key);
+      THEN("The key must not be inside the hash set") {
+        REQUIRE(false == set.contains(key));
+      }
+    }
+  }
+}
+
+TEST_CASE("Hash set retrieve all keys") {
+  GIVEN("A hash set ") {
+    qs::hash_set set;
+    const int maxi = 10;
+    qs::vector<qs::string> arr;
+    WHEN("We insert some keys(qs::string)") {
+      for (int i = 0; i < maxi; ++i) {
+        qs::string key("key");
+        auto skey = key.cat(qs::string(i));
+        set.insert(skey);
+        arr.push(skey);
+      }
+      THEN("We should be able to retrieve all the keys") {
+        auto keys = set.get_all();
+        arr.for_each([&keys](const qs::string &val) {
+          bool found = false;
+          keys.for_each([&val, &found](const qs::string &skey) {
+            if (val == skey) {
+              found = true;
+            }
+          });
+          REQUIRE(true == found);
+        });
       }
     }
   }
