7f3e6b85823bfe95e5d9c2bb249b97278dcab34d
ythomop <ythomop@gmail.com>
2021-11-03 23:39:56 +0200

    Format and #include <qs/functions/iterate.hpp>

--- include/qs/functions.hpp
+++ include/qs/functions.hpp
@@ -1,6 +1,7 @@
 #ifndef QS_FUNCTIONS_HPP
 #define QS_FUNCTIONS_HPP
 
+#include "functions/iterate.hpp"
 #include "functions/ops.hpp"
 
 #endif
--- include/qs/functions/iterate.hpp
+++ include/qs/functions/iterate.hpp
@@ -1,12 +1,12 @@
 #ifndef QS_ITERATE_HPP
 #define QS_ITERATE_HPP
 
-
 namespace qs {
 namespace functions {
-template <typename Iterator,typename F> void for_each(Iterator begin,Iterator end,F fn) {
+template <typename Iterator, typename F>
+void for_each(Iterator begin, Iterator end, F fn) {
   for (; begin != end; begin++) {
-      fn(*begin);
+    fn(*begin);
   }
 }
 
