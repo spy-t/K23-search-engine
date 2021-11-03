eb1889bf1cfc188c8a5cf120539a416c7108c93c
Spyros Trifonidis <spyrostrifon@protonmail.com>
2021-11-03 17:31:20 +0200

    implement the copy_uninitialized generic function

--- include/qs/functions/ops.hpp
+++ include/qs/functions/ops.hpp
@@ -8,11 +8,35 @@
 namespace qs {
 namespace functions {
 
+// Copies from an iterator-like S to an iterator-like D assuming that D is
+// initialized storage using a copy assignment operation
 template <class S, class D> inline void copy(S start, S end, D destination) {
   while (start != end) {
     *destination = *start;
     destination++;
     start++;
+  }
+}
+
+// Copies from an iterator-like S to an iterator-like D assuming that D is
+// uninitialized storage using placement new
+template <class S, class D>
+inline void copy_uninitialized(S start, S end, D destination) {
+  using value_type = typename std::iterator_traits<D>::value_type;
+  D iter = destination;
+
+  try {
+    while (start != end) {
+      new (static_cast<void *>(std::addressof(*iter))) value_type(*start);
+      start++;
+      iter++;
+    }
+  } catch (...) {
+    // Prevent a potential memory leak if a constructor throws
+    for (; destination != iter; ++destination) {
+      destination->~value_type();
+    }
+    throw;
   }
 }
 
