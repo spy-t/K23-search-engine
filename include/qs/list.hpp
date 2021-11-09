1c2f0e6d48bf70f0d564965203594e434a5a6e23
Spyros Trifonidis <spyrostrifon@protonmail.com>
2021-11-09 18:28:27 +0200

    improve the linked_list semantics and add more tests

--- include/qs/list.hpp
+++ include/qs/list.hpp
@@ -11,49 +11,34 @@ namespace qs {
 template <class V> class linked_list;
 
 template <class V> class list_node {
+
   V value;
   list_node<V> *next_node;
   list_node<V> *prev_node;
-  linked_list<V> *list;
 
-  friend class linked_list<V>;
+  friend linked_list<V>;
+  friend class linked_list<list_node<V>>;
+  explicit list_node(const V value)
+      : value(value), next_node(nullptr), prev_node(nullptr) {}
 
-  explicit list_node(const V value, linked_list<V> *list)
-      : value(value), next_node(nullptr), prev_node(nullptr), list(list) {
-    if (list == nullptr) {
-      throw std::runtime_error(
-          "can't instantiate a list node without an associated list");
-    }
-  }
-  explicit list_node(const V value, linked_list<V> *list, list_node<V> *prev,
-                     list_node<V> *next)
-      : value(value), next_node(next), prev_node(prev), list(list) {
-    if (list == nullptr) {
-      throw std::runtime_error(
-          "can't instantiate a list node without an associated list");
-    }
-  }
-
-  bool is_part_of(linked_list<V> *other_list) { return list == other_list; }
+  explicit list_node(const V value, list_node<V> *prev, list_node<V> *next)
+      : value(value), next_node(next), prev_node(prev) {}
 
 public:
   list_node() = delete;
 
-  const list_node<V> *next() const { return next_node; }
-  const list_node<V> *prev() const { return prev_node; }
+  list_node<V> *next() const { return next_node; }
+  list_node<V> *prev() const { return prev_node; }
 
-  // non const variants
-  list_node<V> *next_m() const { return next_node; }
-  list_node<V> *prev_m() const { return prev_node; }
-
-  V get() const { return value; }
+  V &get() { return value; }
+  V &operator*() { return this->get(); }
+  bool operator==(const list_node<V> &other) {
+    return this->value == other.value && this->next_node == other.next_node &&
+           this->prev_node == other.prev_node;
+  }
 };
 
 template <class V> class linked_list {
-  list_node<V> *head_node;
-  list_node<V> *tail_node;
-  std::size_t size;
-
   // Assumes the new node's pointers have been set in the constructor
   void insert_before(list_node<V> &node, list_node<V> &new_node) {
     auto prev = node.prev_node;
@@ -80,86 +65,66 @@ template <class V> class linked_list {
   }
 
 public:
-  linked_list() : head_node(nullptr), tail_node(nullptr), size(0) {}
+  list_node<V> *head;
+  list_node<V> *tail;
+  std::size_t size;
+
+  linked_list() : head(nullptr), tail(nullptr), size(0) {}
 
   // No copy
   linked_list(const linked_list<V> &other) = delete;
   linked_list<V> &operator=(const linked_list<V> &other) = delete;
   ~linked_list() {
-    auto iter = head_node;
+    auto iter = this->head;
     while (iter != nullptr) {
       auto tmp = iter;
-      iter = iter->next_node;
+      iter = iter->next();
       delete tmp;
     }
   }
 
-  const list_node<V> *head() const { return head_node; }
-  const list_node<V> *tail() const { return tail_node; }
-
-  // non const variants
-  list_node<V> *head_m() const { return head_node; }
-  list_node<V> *tail_m() const { return tail_node; }
-
   std::size_t get_size() const { return size; }
 
-  list_node<V> *append(const V value) {
-    auto node = new list_node<V>(value, this, tail_node, nullptr);
-    if (head_node == nullptr && tail_node == nullptr) {
-      head_node = node;
-      tail_node = node;
+  list_node<V> &append(const V value) {
+    auto node = new list_node<V>(value, tail, nullptr);
+    if (head == nullptr && tail == nullptr) {
+      head = node;
+      tail = node;
       ++size;
-      return node;
+      return *node;
     }
-    tail_node->next_node = node;
-    tail_node = node;
+    tail->next_node = node;
+    tail = node;
 
     ++size;
-    return node;
+    return *node;
   }
 
-  list_node<V> *append(V value, list_node<V> *node) {
-    if (node == nullptr) {
-      throw std::invalid_argument("can't append after a null node");
-    }
-    if (!node->is_part_of(this)) {
-      throw std::runtime_error(
-          "can't append after a node that is not a member of this list");
-    }
-    auto new_node = new list_node<V>(value, this, node, node->next_node);
-    insert_after(*node, *new_node);
+  list_node<V> &append(V value, list_node<V> &node) {
+    auto new_node = new list_node<V>(value, std::addressof(node), node.next());
+    insert_after(node, *new_node);
 
-    if (node == tail_node) {
-      tail_node = new_node;
+    if (node == *tail) {
+      tail = new_node;
     }
 
     ++size;
-    return new_node;
+    return *new_node;
   }
-  list_node<V> *prepend(V value, list_node<V> *node) {
-    if (node == nullptr) {
-      throw std::invalid_argument("can't append after a null node");
-    }
-    if (!node->is_part_of(this)) {
-      throw std::runtime_error(
-          "can't append after a node that is not a member of this list");
-    }
-    auto new_node = new list_node<V>(value, this, node->prev_node, node);
-    insert_before(*node, *new_node);
+  list_node<V> &prepend(V value, list_node<V> &node) {
+    auto new_node =
+        new list_node<V>(value, node.prev_node, std::addressof(node));
+    insert_before(node, *new_node);
 
-    if (node == head_node) {
-      head_node == new_node;
+    if (node == *head) {
+      head = new_node;
     }
 
     ++size;
-    return new_node;
+    return *new_node;
   }
 
   void remove(list_node<V> *node) {
-    if (!node->is_part_of(this)) {
-      throw std::runtime_error(
-          "can't remove a node that is not a member of this list");
-    }
     auto prev = node->prev_node;
     auto next = node->next_node;
     if (prev != nullptr) {
@@ -169,12 +134,12 @@ public:
       next->prev_node = prev;
     }
 
-    if (head_node == node) {
-      head_node = next;
+    if (head == node) {
+      head = next;
     }
 
-    if (tail_node == node) {
-      tail_node = prev;
+    if (tail == node) {
+      tail = prev;
     }
 
     --size;
@@ -187,37 +152,96 @@ public:
   }
 
   struct iterator {
-    using iterator_category = std::forward_iterator_tag;
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type = std::ptrdiff_t;
     using value_type = list_node<V>;
     using pointer = value_type *;
     using reference = value_type &;
 
-    pointer cur_p;
+    pointer p;
 
   public:
-    explicit iterator(pointer p) : cur_p(p){};
+    explicit iterator(pointer p) : p(p){};
 
-    reference operator*() { return *cur_p; };
-    pointer operator->() { return cur_p; };
+    reference operator*() { return *p; };
+    pointer operator->() { return p; };
     iterator &operator++() {
-      cur_p = cur_p->next_node;
+      p = p->next_node;
       return *this;
-    };
+    }
     iterator operator++(int) {
       iterator tmp = *this;
-      cur_p = cur_p->next_node;
+      p = p->next_node;
       return tmp;
     }
+
+    iterator &operator--() {
+      p = p->prev_node;
+      return *this;
+    }
+    iterator operator--(int) {
+      iterator prev = *this;
+      p = p->prev_node;
+      return prev;
+    }
+
     friend bool operator==(const iterator &a, const iterator &b) {
-      return a.cur_p == b.cur_p;
+      return a.p == b.p;
     };
     friend bool operator!=(const iterator &a, const iterator &b) {
-      return a.cur_p != b.cur_p;
+      return a.p != b.p;
     };
   };
 
-  iterator begin() { return iterator(this->head_node); };
+  struct reverse_iterator {
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = list_node<V>;
+    using pointer = value_type *;
+    using reference = value_type &;
+
+    pointer p;
+
+  public:
+    explicit reverse_iterator(pointer p) : p(p){};
+
+    reference operator*() { return *p; };
+    pointer operator->() { return p; };
+    reverse_iterator &operator++() {
+      p = p->prev_node;
+      return *this;
+    }
+    reverse_iterator operator++(int) {
+      reverse_iterator prev = *this;
+      p = p->prev_node;
+      return prev;
+    }
+
+    reverse_iterator &operator--() {
+      p = p->next_node;
+      return *this;
+    }
+    reverse_iterator operator--(int) {
+      reverse_iterator tmp = *this;
+      p = p->next_node;
+      return tmp;
+    }
+
+    friend bool operator==(const reverse_iterator &a,
+                           const reverse_iterator &b) {
+      return a.p == b.p;
+    };
+    friend bool operator!=(const reverse_iterator &a,
+                           const reverse_iterator &b) {
+      return a.p != b.p;
+    };
+  };
+
+  iterator begin() { return iterator(this->head); };
   iterator end() { return iterator(nullptr); };
+
+  reverse_iterator rbegin() { return reverse_iterator(this->tail); };
+  reverse_iterator rend() { return reverse_iterator(nullptr); };
 };
 
 } // namespace qs
--- src/test/list_test.cpp
+++ src/test/list_test.cpp
@@ -1,32 +1,81 @@
 #include "catch_amalgamated.hpp"
-#include "qs/list.hpp"
+#include <qs/list.hpp>
 
-TEST_CASE("List iteration through iterator") {
-  GIVEN("A list with some nodes") {
+TEST_CASE("Linked list behaves as expected") {
+  GIVEN("A linked list") {
+    const int maxi = 10;
+    qs::linked_list<int> l;
+    for (int i = 0; i < maxi; ++i) {
+      l.append(i);
+    }
+    REQUIRE(l.get_size() == maxi);
+
+    WHEN("we append") {
+      l.append(10);
+      THEN("the value has been appended") {
+        REQUIRE(l.get_size() == maxi + 1);
+        REQUIRE(**l.tail == 10);
+      }
+    }
+    WHEN("we remove") {
+      l.remove(l.head);
+      THEN("the node has been removed") {
+        REQUIRE(l.get_size() == maxi - 1);
+        REQUIRE(**l.head == 1);
+      }
+    }
+    WHEN("we append after") {
+      l.append(-1, *l.head);
+      THEN("the node has been appended after the given node") {
+        REQUIRE(l.get_size() == maxi + 1);
+        REQUIRE(**l.head->next() == -1);
+      }
+    }
+
+    WHEN("we prepend") {
+      auto prev_head = l.head;
+      l.prepend(-1, *l.head);
+      THEN("the node has been prepended before the given node") {
+        REQUIRE(l.get_size() == maxi + 1);
+        REQUIRE(**l.head == -1);
+        REQUIRE(l.head->next() == prev_head);
+      }
+    }
+  }
+}
+
+TEST_CASE("Linked list iterator behaves as expected") {
+  GIVEN("A linked list") {
     const int maxi = 10;
     qs::linked_list<int> l;
     for (int i = 0; i < maxi; i++) {
       l.append(i);
     }
     WHEN("We iterate forward with prefix iterator") {
-      THEN("We must see all the nodes with the right values") {
+      THEN("We find the expected values") {
         int i = 0;
         for (auto n : l) {
-          REQUIRE(i == n.get());
-          i++;
+          REQUIRE(i++ == n.get());
         }
         REQUIRE(i - 1 == maxi - 1);
       }
     }
     WHEN("We iterate forward with postfix iterator") {
       int i = 0;
-      THEN("We must see all the nodes with the right values") {
+      THEN("We find the expected values") {
         for (auto it = l.begin(), end = l.end(); it != end; it++) {
-          const auto tit = *it;
-          REQUIRE(i == tit.get());
-          i++;
+          REQUIRE(i++ == **it);
         }
         REQUIRE(i - 1 == maxi - 1);
+      }
+    }
+
+    WHEN("We iterate backwards") {
+      int i = 9;
+      THEN("We find the expected values") {
+        for (auto it = l.rbegin(), end = l.rend(); it != end; it++) {
+          REQUIRE(i-- == **it);
+        }
       }
     }
   }
