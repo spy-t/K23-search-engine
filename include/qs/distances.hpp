b70c12e4fb876b005b99f228392f05dd63dae3dd
ythomop <ythomop@gmail.com>
2021-11-11 10:14:32 +0200

    Add edit distance

--- include/qs/distances.hpp
+++ include/qs/distances.hpp
@@ -20,7 +20,49 @@ int hamming_distance(qs::string s1, qs::string s2) {
   return dist;
 }
 
-// TODO add more distances
+int edit_distance(qs::string s1, qs::string s2) {
+  int l1 = (int)s1.get_length();
+  int l2 = (int)s2.get_length();
+
+  int **d = new int *[l1 + 1];
+  for (int i = 0; i <= l1; i++) {
+    d[i] = new int[l2 + 1];
+  }
+
+  for (int i = 0; i <= l1; i++) {
+    d[i][0] = i;
+  }
+
+  for (int i = 0; i <= l2; i++) {
+    d[0][i] = i;
+  }
+
+  for (int j = 0; j < l2; j++) {
+    for (int i = 0; i < l1; i++) {
+      int substitution_cost = s1[i] == s2[j];
+      int deletion = d[i][j + 1] + 1;
+      int insertion = d[i + 1][j] + 1;
+      int substitution = d[i][j] + substitution_cost;
+
+      int min = deletion;
+      if (insertion < min) {
+        min = insertion;
+      }
+      if (substitution < min) {
+        min = substitution;
+      }
+      d[i + 1][j + 1] = min;
+    }
+  }
+
+  int ret = d[l1][l2];
+  for (int i = 0; i <= l1; i++) {
+    delete[] d[i];
+  }
+  delete[] d;
+
+  return ret;
+}
 
 } // namespace qs
 
--- src/test/distances_test.cpp
+++ src/test/distances_test.cpp
@@ -3,13 +3,27 @@
 #include <qs/distances.hpp>
 #include <qs/string.h>
 
-TEST_CASE("Edit distance") {
-  GIVEN("Two strings: 'hell' and 'felt'") {
+TEST_CASE("Hamming distance") {
+  SECTION("Two strings: 'hell' and 'felt'") {
     REQUIRE(qs::hamming_distance(qs::string("hell"), qs::string("felt")) == 2);
   };
 
-  GIVEN("Two strings: 'when' and 'whenever'") {
+  SECTION("Two strings: 'when' and 'whenever'") {
     REQUIRE(qs::hamming_distance(qs::string("when"), qs::string("whenever")) ==
             4);
+  }
+}
+
+TEST_CASE("Edit distance") {
+  SECTION("Two strings: 'hell' and 'felt'") {
+    REQUIRE(qs::edit_distance(qs::string("hell"), qs::string("felt")) == 2);
+  };
+
+  SECTION("Two strings: 'ac' and 'abc'") {
+    REQUIRE(qs::edit_distance(qs::string("ac"), qs::string("abc")) == 2);
+  };
+
+  SECTION("Two strings: 'abc' and 'bc'") {
+    REQUIRE(qs::edit_distance(qs::string("abc"), qs::string("bc")) == 1);
   }
 }
