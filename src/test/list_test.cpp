7f1478e9f780e95e9fe58ab4402d311850144c42
ythomop <ythomop@gmail.com>
2021-11-14 15:41:49 +0200

    List: Removal from inside the list (neither head nor tail)

--- src/test/list_test.cpp
+++ src/test/list_test.cpp
@@ -24,11 +24,30 @@ TEST_CASE("Linked list behaves as expected") {
         REQUIRE(**l.head == 1);
       }
     }
+
+    WHEN("we remove the third node") {
+      l.remove(l.head->next()->next());
+      THEN("the node has been removed") {
+        REQUIRE(l.get_size() == maxi - 1);
+        REQUIRE(l.head->next()->get() == 1);
+        REQUIRE(l.head->next()->next()->get() == 3);
+      }
+    }
+
     WHEN("we append after") {
       l.append(-1, *l.head);
       THEN("the node has been appended after the given node") {
         REQUIRE(l.get_size() == maxi + 1);
         REQUIRE(**l.head->next() == -1);
+      }
+    }
+
+    WHEN("we append at the end") {
+      l.append(-1, *l.tail);
+      THEN("-1 is found in the end of the list") {
+        REQUIRE(l.get_size() == maxi + 1);
+        REQUIRE(l.tail->get() == -1);
+        REQUIRE(**l.tail->prev() == 9);
       }
     }
 
@@ -39,6 +58,19 @@ TEST_CASE("Linked list behaves as expected") {
         REQUIRE(l.get_size() == maxi + 1);
         REQUIRE(**l.head == -1);
         REQUIRE(l.head->next() == prev_head);
+      }
+    }
+    WHEN("we prepend on the second node") {
+      auto prev_node = l.head->next();
+      l.prepend(-1, *prev_node);
+      THEN("the node has been prepended after the head and before the second node") {
+        REQUIRE(l.get_size() == maxi + 1);
+        auto n = l.head;
+        REQUIRE(n->get() == 0);
+        n = n->next();
+        REQUIRE(n->get() == -1);
+        n = n->next();
+        REQUIRE(n->get() == 1);
       }
     }
   }
