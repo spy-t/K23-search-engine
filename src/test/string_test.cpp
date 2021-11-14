9305c195e69cf989824846b5147edc7644074500
ythomop <ythomop@gmail.com>
2021-11-14 17:32:59 +0200

    Complete qs::string tests

--- src/test/string_test.cpp
+++ src/test/string_test.cpp
@@ -48,13 +48,25 @@ SCENARIO("String copy & move semantics work") {
     qs::string s1((char *)"String 1");
     auto size = s1.get_length();
     WHEN("It is assigned to another variable") {
-      auto s2 = s1;
+      qs::string s2;
+      s2 = s1;
 
       THEN("They are the same string") { REQUIRE(s1 == s2); }
     }
 
-    WHEN("It is move to another variable") {
+    WHEN("It is moved to another variable") {
       qs::string s2(std::move(s1));
+
+      REQUIRE(s2.get_length() == size);
+
+      THEN("The old variable is no longer valid") {
+        REQUIRE(s1.get_length() == 0);
+      }
+    }
+
+    WHEN("It is moved-assigned to another variable") {
+      qs::string s2("irrelevant string");
+      s2 = std::move(s1);
 
       REQUIRE(s2.get_length() == size);
 
@@ -66,6 +78,15 @@ SCENARIO("String copy & move semantics work") {
 }
 
 SCENARIO("String indexing works") {
+  GIVEN("An empty string") {
+    qs:: string s("");
+
+    WHEN("It is dereferenced") {
+      THEN("'String is empty' exception is thrown") {
+        REQUIRE_THROWS_MATCHES(s[0], std::runtime_error, Catch::Matchers::Message("Invalid index. String is empty"));
+      }
+    }
+  }
   GIVEN("A string") {
     qs::string s((char *)"String 1");
 
@@ -84,6 +105,34 @@ SCENARIO("String indexing works") {
       auto c = *s;
 
       THEN("The character matches") { REQUIRE(*c == 'S'); }
+    }
+  }
+}
+
+SCENARIO("String comparisons work") {
+  GIVEN("A string: 'String 1'") {
+    qs::string s1("String 1");
+
+    WHEN("It is compared with a different string: 'String 2'") {
+      qs::string s2("String 2");
+
+      THEN("They are not equal") {
+        REQUIRE_FALSE(s1 == s2);
+        REQUIRE(s1 != s2);
+        REQUIRE_FALSE(s1 == "String 2");
+        REQUIRE(s1 != "String 2");
+      }
+
+      THEN("The first string is lesser than the second") {
+        REQUIRE(s1 < s2);
+        REQUIRE_FALSE(s1 > s2);
+        REQUIRE(s1 <= s2);
+        REQUIRE_FALSE(s1 >= s2);
+        REQUIRE(s1 < "String 2");
+        REQUIRE(s1 <= "String2");
+        REQUIRE_FALSE(s1 > "String 2");
+        REQUIRE_FALSE(s1 >= "String 2");
+      }
     }
   }
 }
