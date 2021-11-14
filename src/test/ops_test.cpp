2a1694b170b5c844787345035d5ab2950e88dcb4
ythomop <ythomop@gmail.com>
2021-11-14 15:43:58 +0200

    qs::functions::find test whether the searched-for value is not in the search set

---
+++ src/test/ops_test.cpp
@@ -0,0 +1,16 @@
+#include "catch_amalgamated.hpp"
+
+#include <qs/functions/ops.hpp>
+#include <qs/vector.hpp>
+
+TEST_CASE("qs::functions::find") {
+  SECTION("Nothing found") {
+    auto v = qs::vector<const char *>();
+    v.push("word1");
+    v.push("word2");
+    auto result = qs::functions::find(v.begin(), v.end(), "word3");
+
+    REQUIRE(*result == *(v.end()));
+  }
+}
+
