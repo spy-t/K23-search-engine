ddfa9ca0a0a54fb8f55b2ba7708442ad344cc5fe
ismagrip <cmagripis@protonmail.com>
2021-11-14 20:06:47 +0200

    basic deduplication test

---
+++ src/test/deduplication_test.cpp
@@ -0,0 +1,25 @@
+#include "catch_amalgamated.hpp"
+
+#include <qs/functions.hpp>
+#include <qs/hash_set.h>
+#include <qs/parser.hpp>
+
+TEST_CASE("Deduplication") {
+  GIVEN("A file containing 10 strings with 5 of them being unique"){
+    const char *filepath = "../src/test/resources/5unique_test.txt";
+    FILE *f = std::fopen(filepath, "r");
+    WHEN("We insert entries into the hash set"){
+      qs::hash_set set;
+      qs::parse_file(f, '\n', [&set](const qs::string &entry) {
+        set.insert(entry);
+      });
+      printf("----------------------------------\n");
+      THEN("Hash set should contains only 5 entries"){
+        auto entries = set.get_all();
+        REQUIRE(entries.get_size() == 5);
+      }
+      std::fclose(f);
+    }
+  }
+
+}
