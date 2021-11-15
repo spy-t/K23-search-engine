51449bb5c1de05a680afbc96f5e13c981fd2059a
ythomop <ythomop@gmail.com>
2021-11-15 20:44:49 +0200

    Optimize edit distance

--- src/lib/distances.cpp
+++ src/lib/distances.cpp
@@ -1,5 +1,7 @@
 #include <qs/distances.hpp>
 
+#include <qs/core.h>
+
 namespace qs {
 
 int hamming_distance(qs::string s1, qs::string s2) {
@@ -23,40 +25,44 @@ int hamming_distance(qs::string s1, qs::string s2) {
   return dist;
 }
 
+static QS_FORCE_INLINE int get(int *arr, int row, int col, int width) {
+  return arr[row * width + col];
+}
+static QS_FORCE_INLINE void set(int *arr, int row, int col, int width, int val) {
+  arr[row * width + col] = val;
+}
+
 int edit_distance(qs::string s1, qs::string s2) {
   int l1 = (int)s1.get_length();
   int l2 = (int)s2.get_length();
 
-  int **d = new int *[l1 + 1];
-  for (int i = 0; i <= l1; i++) {
-    d[i] = new int[l2 + 1];
-  }
+  int height = l1 + 1;
+  int width = l2 + 1;
+
+  int *d = new int [width * height];
 
   for (int i = 0; i <= l1; i++) {
-    d[i][0] = i;
+    set(d, i, 0, width, i);
   }
 
   for (int j = 0; j <= l2; j++) {
-    d[0][j] = j;
+    set(d, 0, j, width, j);
   }
 
   for (int i = 1; i <= l1; i++) {
     for (int j = 1; j <= l2; j++) {
       int substitution_cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
-      int deletion = d[i - 1][j] + 1;
-      int insertion = d[i][j - 1] + 1;
-      int substitution = d[i - 1][j - 1] + substitution_cost;
+      int deletion = get(d, i - 1, j, width) + 1;
+      int insertion = get(d, i, j - 1, width) + 1;
+      int substitution = get(d, i - 1, j - 1, width) + substitution_cost;
 
       int min = qs::functions::min(deletion,
                                    qs::functions::min(insertion, substitution));
-      d[i][j] = min;
+      set(d, i, j, width, min);
     }
   }
 
-  int ret = d[l1][l2];
-  for (int i = 0; i <= l1; i++) {
-    delete[] d[i];
-  }
+  int ret = get(d, l1, l2, width);
   delete[] d;
 
   return ret;
