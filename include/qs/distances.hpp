534b4aa0d5d225fe170c3977bce4e506bbc5fd93
Yannis Thomopoulos <ythomop@gmail.com>
2021-12-12 22:39:45 +0200

    Hamming and edit distance can now stop early

--- include/qs/distances.hpp
+++ include/qs/distances.hpp
@@ -3,11 +3,12 @@
 
 #include <qs/functions/ops.hpp>
 #include <qs/string.h>
+#include <limits>
 
 namespace qs {
 
-int hamming_distance(qs::string s1, qs::string s2);
-int edit_distance(const qs::string &s1, const qs::string &s2);
+int hamming_distance(const qs::string &s1, const qs::string &s2, int max = std::numeric_limits<int>::max());
+int edit_distance(const qs::string &s1, const qs::string &s2, int max = std::numeric_limits<int>::max());
 
 } // namespace qs
 
--- src/lib/distances.cpp
+++ src/lib/distances.cpp
@@ -4,24 +4,20 @@
 
 namespace qs {
 
-int hamming_distance(qs::string s1, qs::string s2) {
-  int dist = 0;
-  std::size_t len1 = s1.length();
-  std::size_t len2 = s2.length();
-  if (len1 != len2) {
+int hamming_distance(const qs::string &s1, const qs::string &s2, int max) {
+  if (s1.length() != s2.length()) {
     // TODO(spyros): This should be an assertion. Change this when the assertion
     // system is in place
     throw std::runtime_error("cannot find hamming distance between two strings "
-                             "of differenct lengths");
+                             "of different lengths");
   }
-  std::size_t min_len = qs::functions::min(len1, len2);
-
-  for (std::size_t i = 0; i < min_len; i++) {
-    if (s1[i] != s2[i]) {
-      dist++;
-    }
+  int dist = 0;
+  std::size_t len = s1.length();
+  auto *c1 = s1.data();
+  auto *c2 = s2.data();
+  for (std::size_t i = 0; i < len && max >= dist; i++) {
+    dist += c1[i] != c2[i];
   }
-  dist += int(len1 < len2 ? len2 - len1 : len1 - len2);
   return dist;
 }
 
@@ -35,7 +31,7 @@ static QS_FORCE_INLINE void init_edit_buffer(int *buffer, int len) {
 #define EDIT_BUFFER_SIZE 64ul
 #endif
 
-int edit_distance(const qs::string &s1, const qs::string &s2) {
+int edit_distance(const qs::string &s1, const qs::string &s2, int max) {
   static int d[EDIT_BUFFER_SIZE];
   int max_len = (int)s1.length();
   auto *max_str = s1.data();
@@ -69,12 +65,17 @@ int edit_distance(const qs::string &s1, const qs::string &s2) {
   for (int i = 1; i <= min_len; i++) {
     d[0] = i;
     int prev = i - 1;
+    int best = i;
     for (int j = 1; j <= max_len; j++) {
       int sub = prev + (int)(min_str[i - 1] != max_str[j - 1]);
       int del = d[j - 1];
       int ins = d[j];
       prev = d[j];
       d[j] = functions::min(sub, functions::min(del, ins) + 1);
+      best = functions::min(best, d[j]);
+    }
+    if (best > max) {
+      return best;
     }
   }
 
