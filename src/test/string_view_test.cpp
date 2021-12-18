9d7daf39aec8df18e6c002af9de7da96d5d8b340
Spyros Trifonidis <spyrostrifon@protonmail.com>
2021-12-18 19:56:22 +0200

    add more edge case tests to string_view

--- src/test/string_view_test.cpp
+++ src/test/string_view_test.cpp
@@ -15,11 +15,38 @@ TEST_CASE("string_view behaves as expected", "[string_view]") {
   auto a = s.split(' ');
   REQUIRE(a == "a");
   REQUIRE(s == "string");
-  REQUIRE(s.split(' ') == qs::string_view::empty);
+  REQUIRE(s.split(' ') == "string");
 
-  qs::string_view s1{"http dbpedia resource sokol airport http dbpedia ontology abstract sokol airport arport sokol magadan oblast russia airport located north magadan city center irport sometimes confused with dolinsk sokol base which home fighters that shot down korean flight town gained exposure western world with inauguration alaska airlinef flights united states using mcdonnell douglas jets according anecdotal story published york times first alaska airliues flight needed deicing services which were unavailable flight crew acquired quantity vodka sprayed onto wings airline threatened discontinue russian service difficulties with contract workers alaska airlines flights into magadan elsewhere russia were halted october shortly after russian financial crisis which rendered routes unprofitable aeroflot suspended flights sokol airpfrt february planned removal from service aircraft aeroflot cited lack certification airpoirt acceptance servicing more modern aircraft such airbus airbus primary reason suspension flights resumed service march http wikipedia wiki sokol arport"};
+  qs::string_view s1{
+      "http dbpedia resource sokol airport http dbpedia ontology abstract "
+      "sokol airport arport sokol magadan oblast russia airport located north "
+      "magadan city center irport sometimes confused with dolinsk sokol base "
+      "which home fighters that shot down korean flight town gained exposure "
+      "western world with inauguration alaska airlinef flights united states "
+      "using mcdonnell douglas jets according anecdotal story published york "
+      "times first alaska airliues flight needed deicing services which were "
+      "unavailable flight crew acquired quantity vodka sprayed onto wings "
+      "airline threatened discontinue russian service difficulties with "
+      "contract workers alaska airlines flights into magadan elsewhere russia "
+      "were halted october shortly after russian financial crisis which "
+      "rendered routes unprofitable aeroflot suspended flights sokol airpfrt "
+      "february planned removal from service aircraft aeroflot cited lack "
+      "certification airpoirt acceptance servicing more modern aircraft such "
+      "airbus airbus primary reason suspension flights resumed service march "
+      "http wikipedia wiki sokol arport"};
   auto spl = s1.split(' ');
   REQUIRE(spl == "http");
+
+  SECTION("complex split") {
+    qs::string_view s{"a big separated"};
+    auto a = s.split(' ');
+    auto b = s.split(' ');
+    auto c = s.split(' ');
+
+    REQUIRE(a == "a");
+    REQUIRE(b == "big");
+    REQUIRE(c == "separated");
+  }
 }
 
 TEST_CASE("String view hash works", "[string_view]") {
