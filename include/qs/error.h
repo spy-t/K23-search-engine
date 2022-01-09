d7e241de986377b9da2dad15464c7b31183ee2a2
Yannis Thomopoulos <ythomop@gmail.com>
2022-01-09 23:52:32 +0200

    Error handling macros

--- include/qs/error.h
+++ include/qs/error.h
@@ -1,6 +1,34 @@
 #ifndef QS_ERROR_H
 #define QS_ERROR_H
 
-#define QS_TRACE_ERR(err) __FILE__ ":" __func__ ":" __LINE__ ": " #err
+#define QS_TRACE_ERR(op)                                                       \
+  do {                                                                         \
+    int err = (op);                                                            \
+    if (err) {                                                                 \
+      fprintf(stderr, "Error @ %s:%d : operation: '%s', error: %d - %s",       \
+              __FILE__, __LINE__, #op, err, std::strerror(err));               \
+    }                                                                          \
+  } while (false);
+
+#define QS_RETURN_IF_ERR(op)                                                   \
+  do {                                                                         \
+    int err = (op);                                                            \
+    if (err) {                                                                 \
+      fprintf(stderr, "Error @ %s:%d : operation: '%s', error: %d - %s",       \
+              __FILE__, __LINE__, #op, err, std::strerror(err));               \
+      return err;                                                              \
+    }                                                                          \
+  } while (false);
+
+#define QS_RETURN_POINTER_IF_ERR(op)                                           \
+  do {                                                                         \
+    int err = (op);                                                            \
+    if (err) {                                                                 \
+      fprintf(stderr, "Error @ %s:%d : operation: '%s', error: %d - %s",       \
+              __FILE__, __LINE__, #op, err, std::strerror(err));               \
+      auto ret = new int(err);                                                 \
+      return ret;                                                              \
+    }                                                                          \
+  } while (false);
 
 #endif
