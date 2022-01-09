96b145af79b08410f15fbb38fb46aef0513a650e
Yannis Thomopoulos <ythomop@gmail.com>
2022-01-09 23:52:58 +0200

    qs::job fixes

--- include/qs/job.hpp
+++ include/qs/job.hpp
@@ -1,13 +1,18 @@
 #ifndef QS_SEARCH_JOB_H
 #define QS_SEARCH_JOB_H
 
-#include <functional>
-
 namespace qs {
 
 struct job {
-  std::function<void *(void * args)> f;
+  std::function<void *(void *args)> f;
   void *args;
+
+  job() {
+    this->f = [](void *a) { return a; };
+    this->args = nullptr;
+  }
+  job(std::function<void *(void *args)> f, void *args)
+      : f(std::move(f)), args(args) {}
 };
 
 } // namespace qs
